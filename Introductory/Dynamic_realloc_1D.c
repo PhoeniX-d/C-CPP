9c2167ff862dcdae7388d6ea7ed9b6f1b9a4a19b
PhoeniX-d <pranav24choudhary@outlook.com>
2020-08-13 01:11:51 +0530

    Dynamic Memory Alloction

---
+++ Introductory/Dynamic_realloc_1D.c
@@ -0,0 +1,60 @@
+/*
+*   Program to create one dimensional array using dynamic memory allocation
+*   using library function realloc()
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+void OneDimensional(int);
+
+int main()
+{
+    int iNum = 0;
+    printf("%d", sizeof(&iNum));
+    printf("Enter number of elements\n");
+    scanf("%d", &iNum);
+    OneDimensional(iNum);
+    return 0;
+}
+///////////////////////////////////////////////////////////////////
+//
+//  Name        :OneDimensional
+//  Input       :int
+//  Returns     :void
+//  Description :Creates OneDimensional array, uses it, deallocate
+//               it using realloc
+//  Author      :Pranav P. Choudhary
+//  Date        :13 August 2020
+//
+//////////////////////////////////////////////////////////////////
+void OneDimensional(
+                        int iNum    // Max Elements
+                    )
+{
+    if(iNum == 0)
+        return;
+    int *iArr = NULL, i = 0;
+    if(iNum < 0)
+        iNum = -iNum;
+
+    // allocation
+    iArr = (int *)realloc(NULL, sizeof(int) * iNum);
+    if(NULL == iArr)
+    {
+        printf("Memory Allocation Failed!\n");
+        return;
+    }
+    // use
+    printf("Enter the elements\n");
+    for (i = 0; i < iNum; i++)
+    {
+        scanf("%d", &iArr[i]);
+    }
+    printf("Enterd Elements are\n");
+    for (i = 0; i < iNum; i++)
+    {
+        printf("%d\n", iArr[i]);
+    }
+    // deallocation
+    realloc(iArr, 0);
+}
