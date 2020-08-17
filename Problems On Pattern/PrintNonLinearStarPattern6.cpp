3280a526a4c30851764fed3c55771f0175421d16
PhoeniX-d <prnv24choudhary@gmail.com>
2020-08-17 23:10:20 +0530

    Problems on Pattern Non Linear

--- Problems On Pattern/PrintNonLinearStarPattern6.cpp
+++ Problems On Pattern/PrintNonLinearStarPattern6.cpp
@@ -2,7 +2,9 @@
 *   Program to print designated star patterns
 */
 #include<iostream>
+#include<stdio.h>
 #include<iomanip>
+#include<string.h>
 
 using namespace std;
 class Pattern
@@ -16,20 +18,17 @@ class Pattern
         {
             iRow = iRows;
         }
-        Pattern(string s)
-        {
-        	cStr = (char*)malloc(sizeof(char) * strlen(s));
-        	cStr = s;
-        }
+
         void Updater(int);
         void PrintPattern1();
         void PrintPattern2();
         void PrintPattern3();
         void PrintPattern4();
-        void PrintPattern5();
-        void PrintPattern6();
-        void PrintPattern7();
-        void PrintPattern8();
+        void PrintPattern5(char*);
+        void PrintPattern6(char*);
+        void PrintPattern7(char*);
+        void PrintPattern8(char*);
+        void PrintPattern9(char*);
 };
 
 ////////////////////////////////////////////////////////////////////////
@@ -190,22 +189,26 @@ void Pattern::PrintPattern4()
 //	Input		:void
 //	Returns		:void
 //	Description	:print given pattern
-//
+//                  P
+//                  P r
+//                  P r a
+//                  P r a n
+//                  P r a n a
+//                  P r a n a v
 //	Author		:Pranav Choudhary
 //	Date		:16 August 2020
 //
 ////////////////////////////////////////////////////////////////////////
-void Pattern::PrintPattern5()
+void Pattern::PrintPattern5(char *str)
 {
-    int i = 0, j = 0;
-    Updater(this->iRow);
-    for (i = 1; i <= iRow; i++)
+    int i = 0, j = 0, iLen = strlen(str);
+    for (i = 0; i < iLen; i++)
     {
-        for (j = 1; j <= i; j++)
+        for (j = 0; j <= i; j++)
         {
-            cout << i << " ";
+            cout << str[j] << " ";
         }
-            cout << endl;
+        cout << endl;
     }
 }
 
@@ -215,26 +218,27 @@ void Pattern::PrintPattern5()
 //	Input		:void
 //	Returns		:void
 //	Description	:print given pattern
-//                1
-//                1 2
-//                1 2 3
-//                1 2 3 4
-//                1 2 3 4 5
+//                  P r a n a v
+//                  P r a n a
+//                  P r a n
+//                  P r a
+//                  P r
+//                  P
 //	Author		:Pranav Choudhary
 //	Date		:16 August 2020
 //
 ////////////////////////////////////////////////////////////////////////
-void Pattern::PrintPattern6()
+void Pattern::PrintPattern6(char *str)
 {
-    int i = 0, j = 0;
-    Updater(this->iRow);
-    for (i = 1; i <= iRow; i++)
+    int i = 0, j = 0, iLen = strlen(str);
+    for (i = 0; i < iLen; i++)
     {
-        for (j = 1; j <= i; j++)
+        for (j = 0; j < iLen; j++)
         {
-            cout << j << " ";
+            if(j < iLen - i)
+                cout << str[j] << " ";
         }
-            cout << endl;
+        cout << endl;
     }
 }
 
@@ -244,26 +248,31 @@ void Pattern::PrintPattern6()
 //	Input		:void
 //	Returns		:void
 //	Description	:print given pattern
-//               1
-//               2  3
-//               4  5  6
-//               7  8  9  10
-//               11 12 13 14 15
+//                  P r A n A v
+//                  P r A n A
+//                  P r A n
+//                  P r A
+//                  P r
+//                  P
 //	Author		:Pranav Choudhary
 //	Date		:16 August 2020
 //
 ////////////////////////////////////////////////////////////////////////
-void Pattern::PrintPattern7()
+void Pattern::PrintPattern7(char *str)
 {
-    int i = 0, j = 0, iNum = 1;
-    Updater(this->iRow);
-    for (i = 1; i <= iRow; i++)
+    int i = 0, j = 0, iLen = strlen(str);
+    for (i = 0; i < iLen; i++)
     {
-        for (j = 1; j <= i; j++, iNum++)
+        for (j = 0; j < iLen - i; j++)
         {
-            cout << setw(2) << iNum << " ";
+            if(j % 2 == 0)
+            {
+                if(str[j] >= 'a' && str[j] <= 'z')
+                    str[j] = str[j] - 32;
+            }
+            cout << str[j] << " ";
         }
-            cout << endl;
+        cout << endl;
     }
 }
 
@@ -273,32 +282,105 @@ void Pattern::PrintPattern7()
 //	Input		:void
 //	Returns		:void
 //	Description	:print given pattern
-//                  1
-//                  4  9
-//                  16 25 36
+//                    U
+//                    U N
+//                    U N I
+//                    U N I X
+//                    W
+//                    W I
+//                    W I N
+//                    W I N 3
+//                    W I N 3 2
+//                    S
+//                    S D
+//                    S D K
 //	Author		:Pranav Choudhary
-//	Date		:16 August 2020
+//	Date		:17 August 2020
 //
 ////////////////////////////////////////////////////////////////////////
-void Pattern::PrintPattern8()
+void Pattern::PrintPattern8(char *str)
 {
-    int i = 0, j = 0, iNum = 1;
-    Updater(this->iRow);
-    for (i = 1; i <= iRow; i++)
+    int i = 0, j = 0, iLen = strlen(str);
+    char *temp = str;
+    for (i = 1; i <= iLen; i++)
     {
-        for (j = 1; j <= i; j++, iNum++)
+        for (j = 1; j <= i; j++)
         {
-            cout << setw(3) << iNum * iNum << " ";
+            cout << *temp << " ";
+            temp++;
+            if(*temp == ' ')
+            {
+                str = temp + 1;
+                iLen = iLen - (i + 1);
+                i = 0;
+            }
         }
-            cout << endl;
+        temp = str;
+        cout << endl;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
+//
+//	Name		:PrintPattern9
+//	Input		:char *
+//	Returns		:void
+//	Description	:print given pattern
+//                U
+//                U N
+//                U N I
+//                U N I X
+//                      W 
+//                      W I
+//                      W I N
+//                      W I N 3
+//                      W I N 3 2
+//                              S
+//                              S D
+//                              S D K              
+//	Author		:Pranav Choudhary
+//	Date		:17 August 2020
+//
+////////////////////////////////////////////////////////////////////////
+void Pattern::PrintPattern9(char *str)
+{
+    int i = 0, j = 0, iLen = strlen(str), iCnt1 = 0, iCnt2 = 0;
+    char *temp = str;
+    for (i = 1; i <= iLen; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            while(iCnt1 != 0)
+            {
+                cout << "  ";
+                iCnt1--;
+            }
+            cout << *temp << " ";
+            temp++;
+            if(*temp == ' ')
+            {
+                str = temp + 1;
+                iLen = iLen - ++i;
+                i = 0;
+                iCnt2 = iCnt2 + (j - 1);
+            }
+        }
+        temp = str;
+        iCnt1 = iCnt2;
+        cout << endl;
     }
 }
 
 int main()
 {
     int iValue1 = 0, iValue2 = 0;
+    char cStr[30] = {'\0'};
+
     cout << "Enter the number of rows\n";
     cin >> iValue1;
+
+    cout << "\nEnter the string\n";
+    scanf(" %[^\n]", cStr);
 
     Pattern pObj(iValue1);
     cout << "\n-----Pattern 1-----\n";
@@ -308,15 +390,17 @@ int main()
     cout << "\n-----Pattern 3-----\n";
     pObj.PrintPattern3();
     cout << "\n-----Pattern 4-----\n";
-    pObj.PrintPattern4();    
-   /* cout << "\n-----Pattern 5-----\n";
-    pObj.PrintPattern5();
+    pObj.PrintPattern4(); 
+    cout << "\n-----Pattern 5-----\n";
+    pObj.PrintPattern5(cStr);
     cout << "\n-----Pattern 6-----\n";
-    pObj.PrintPattern6();
+    pObj.PrintPattern6(cStr);
     cout << "\n-----Pattern 7-----\n";
-    pObj.PrintPattern7();
+    pObj.PrintPattern7(cStr);
     cout << "\n-----Pattern 8-----\n";
-    pObj.PrintPattern8();*/
-
+    pObj.PrintPattern8(cStr);
+    cout << "\n-----Pattern 9-----\n";
+    pObj.PrintPattern9(cStr);
+    
     return 0;
 }
