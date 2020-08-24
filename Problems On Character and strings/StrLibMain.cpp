27945c7151f7631601ae30127d4ced75eacf1360
PhoeniX-d <pranav24choudhary@outlook.com>
2020-08-24 11:43:38 +0530

    StrLib Edit 3

--- Problems On Character and strings/StrLibMain.cpp
+++ Problems On Character and strings/StrLibMain.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include "StrLib.cpp"
 
 int main()
@@ -12,19 +12,4 @@ int main()
     Strings sObj; 
 
     return 0;
-=======
-#include "StrLib.cpp"
-
-int main()
-{
-    char cArr[MAXLEN] = {'\0'};
-
-    cout << "Enter the string\n" ;    
-    cin.get(cArr, MAXLEN, '\n');
-    cout << cArr << endl;
-
-    Strings sObj; 
-
-    return 0;
->>>>>>> 717fffa1367939d151056f7431e96ae847d6171b
 }
