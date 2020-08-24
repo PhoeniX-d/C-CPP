717fffa1367939d151056f7431e96ae847d6171b
Pranav Choudhary <57325714+PhoeniX-d@users.noreply.github.com>
2020-08-24 11:38:00 +0530

    String Library Edit 2

---
+++ Problems On Character and strings/StrLibMain.cpp
@@ -0,0 +1,14 @@
+#include "StrLib.cpp"
+
+int main()
+{
+    char cArr[MAXLEN] = {'\0'};
+
+    cout << "Enter the string\n" ;    
+    cin.get(cArr, MAXLEN, '\n');
+    cout << cArr << endl;
+
+    Strings sObj; 
+
+    return 0;
+}
