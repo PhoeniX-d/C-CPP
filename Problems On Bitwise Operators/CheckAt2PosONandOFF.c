393c3438ee91924ec46934fddec084cd8e501985
PhoeniX-d <pranav24choudhary@outlook.com>
2020-08-28 10:58:19 +0530

    Problems On Bits

---
+++ Problems On Bitwise Operators/CheckAt2PosONandOFF.c
@@ -0,0 +1,85 @@
+/*
+*   Write a program which accept one number ,two positions from user and
+*   check whether bit at first and bit at second position is ON or OFF.
+*   Input : 10 3 7
+*   Output : TRUE 
+*/
+
+#include<stdio.h>
+
+#define TRUE    1
+#define FALSE   0
+#define ERROR  -1
+
+typedef unsigned int UINT;
+typedef char BOOL;
+
+BOOL CheckONandOFF(UINT, int, int);
+
+int main()
+{
+    UINT uNum = 0;
+    int iBit1 = 1, iBit2 = 0;
+    BOOL bRet = FALSE;
+
+    printf("Enter the number:\n");
+    scanf("%u", &uNum);
+
+    printf("Enter 1st Bit position:\n");
+    scanf("%d", &iBit1);
+
+    printf("Enter 2nd Bit position:\n");
+    scanf("%d", &iBit2);
+
+    bRet = CheckONandOFF(uNum, iBit1, iBit2);
+    if(bRet == TRUE)
+    {
+        printf("TRUE\n");
+    }
+    else if(bRet == FALSE)
+    {
+        printf("FALSE\n");
+    }
+    else
+    {
+        printf("Invalid Input\n");
+    }
+    
+    return 0;
+}
+
+//////////////////////////////////////////////////////////////////////
+//
+//  Name        :CheckONandOFF
+//  Input       :UINT, int, int
+//  Returns     :BOOL
+//  Description :check whether bit at first and bit at second position
+//               is ON or OFF
+//  Author      :Pranav Choudhary
+//  Date        :28 Aug 2020
+//
+//////////////////////////////////////////////////////////////////////
+BOOL CheckONandOFF(UINT uNum, int iBit1, int iBit2)
+{
+    if(iBit1 < 1 || iBit1 > 32 || iBit2 < 1 || iBit2 > 32)
+    {
+        return ERROR;
+    }
+
+    UINT uMask1 = 1, uMask2 = 1, uMask = 0, uRes = 0;
+
+    uMask1 = uMask1 << (iBit1 - 1);
+    uMask2 = uMask2 << (iBit2 - 1);
+
+    uMask = uMask1 | uMask2;
+    uRes = uMask & uNum;
+
+    if(uRes == uMask)
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }    
+}
--- Problems On Bitwise Operators/ConvertToBinary.c
+++ Problems On Bitwise Operators/ConvertToBinary.c
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
 *   Program to Display Binary representation of enterd number
 */
@@ -47,53 +47,3 @@ void PrintBin(UINT uNum)
         }
     }
 }
-=======
-/*
-*   Program to Display Binary representation of enterd number
-*/
-#include<stdio.h>
-#define MAXLEN      32
-
-typedef unsigned int UINT;
-
-void PrintBin(UINT);
-
-int main()
-{
-    UINT uNum = 0;
-    printf("Enter the number\n");
-    scanf("%lu", &uNum);
-    printf("Output:\n");
-    PrintBin(uNum);
-
-    return 0;
-}
-//////////////////////////////////////////////////////////////
-//
-//  Name        :PrintBin
-//  Input       :UINT
-//  Returns     :void
-//  Description :Converts UINT to equivalent binary
-//  Author      :Pranav Choudhary
-//  Date        :27 Aug 2020
-//
-//////////////////////////////////////////////////////////////
-void PrintBin(UINT uNum)
-{
-    char cBin[MAXLEN] = {0}, i = 0, j = 0;
-    while(uNum != 0)
-    {
-        cBin[i] = uNum % 2;
-        uNum = uNum / 2;
-        i++;
-    }
-    for (j = 31; j >= 0; j--)
-    {
-        printf("%d", cBin[j]);
-        if(j % 4 == 0)
-        {
-            printf(" ");
-        }
-    }
-}
->>>>>>> 06cbc0ab9a0e44537ee7ca6c2b6e2967607bd842
