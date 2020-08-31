9a190cca89ebb2649cce38fd527705090a61c6d0
PhoeniX-d <pranav24choudhary@outlook.com>
2020-08-31 13:09:22 +0530

    Circular Linked List C

--- Problems On Data Structures/SLLL_CircularLL.c
+++ Problems On Data Structures/SLLL_CircularLL.c
@@ -38,6 +38,32 @@ int main()
     printf("Linked List:InsertFirst\n");
     DisplayList(Head, Tail);   
 
+    InsertLast(&Head, &Tail, 34);
+    InsertLast(&Head, &Tail, 56);
+    InsertLast(&Head, &Tail, 24);
+    InsertLast(&Head, &Tail, 900);
+
+    printf("Linked List:InsertLast\n");
+    DisplayList(Head, Tail);
+
+    InsertAtPos(&Head, &Tail, 233, 9);
+    InsertAtPos(&Head, &Tail, 2235, 3);
+
+    printf("Linked List:InsertAtPos\n");
+    DisplayList(Head, Tail);
+    
+    DeleteFirst(&Head, &Tail);
+    printf("Linked List:DeleteFirst\n");
+    DisplayList(Head, Tail);
+
+    DeleteLast(&Head, &Tail);
+    printf("Linked List:DeleteLast\n");
+    DisplayList(Head, Tail);
+
+    DeleteAtPos(&Head, &Tail, 4);
+    printf("Linked List:DeleteAtPos\n");
+    DisplayList(Head, Tail);
+
     iRet = CountEls(Head, Tail);
     printf("Total Elements:\n%d\n", iRet);
     
@@ -69,7 +95,6 @@ void DisplayList(PNODE First, PNODE Last)
         printf("%d->", First->iData);
         First = First->npNext;
     } while (First != Last->npNext);
-
     printf("\n\n");
 }// end of DisplayList
 
@@ -166,3 +191,199 @@ void InsertFirst(PPNODE First, PPNODE Last, int iNum)
     (*Last)->npNext = *First;
 
 }// End of InsertFirst
+
+////////////////////////////////////////////////////////////
+//
+//  Name        :InsertLast
+//  Input       :PPNODE, PPNODE, int
+//  Returns     :void
+//  Description :Insert element at last position
+//  Author      :Pranav Choudhary
+//  Date        :31 Aug 2020
+//
+////////////////////////////////////////////////////////////
+void InsertLast(PPNODE First, PPNODE Last, int iNum)
+{
+    PNODE NewN = NULL;
+    NewN = (PNODE)malloc(sizeof(NODE));
+    
+    if(NULL == NewN)
+    {
+        printf("Memory Allocation Failed");
+        return;
+    }
+    
+    NewN->iData = iNum;
+    NewN->npNext = NULL;
+
+     /* if linked list is not empty */
+    if(*First == NULL && *Last == NULL)
+    {
+        *First = NewN;
+    }
+    else
+    {
+        (*Last)->npNext = NewN;
+    }
+    *Last = NewN;
+    (*Last)->npNext = *First;  
+}// End of InsertLast
+
+////////////////////////////////////////////////////////////
+//
+//  Name        :InsertAtPos
+//  Input       :PPNODE, PPNODE, int, int
+//  Returns     :void
+//  Description :Insert element at desired position
+//  Author      :Pranav Choudhary
+//  Date        :31 Aug 2020
+//
+////////////////////////////////////////////////////////////
+void InsertAtPos(PPNODE First, PPNODE Last, int iNum, int iPos)
+{
+    int iSize = CountEls(*First, *Last);
+    if(iPos > iSize + 1 || iPos < 1)
+    {
+        printf("Invalid Position Entered\n");
+        return;
+    }
+    else if(iPos == 1)
+    {
+        InsertFirst(First, Last, iNum);
+        return;
+    }
+    else if(iPos == iSize + 1)
+    {
+        InsertLast(First, Last, iNum);
+        return;
+    }
+    else
+    {
+        PNODE NewN = NULL;
+        PNODE nTemp = *First;
+        int i = 0;
+        NewN = (PNODE)malloc(sizeof(NODE));        
+        if(NULL == NewN)
+        {
+            printf("Memory Allocation Failed");
+            return;
+        }
+        
+        NewN->iData = iNum;
+        NewN->npNext = NULL;
+
+        for (i = 1; i < iPos - 1; i++)
+        {
+            nTemp = nTemp->npNext;
+        }
+        NewN->npNext = nTemp->npNext;
+        nTemp->npNext = NewN;
+    }   
+}// End of InsertAtPos
+
+////////////////////////////////////////////////////////////
+//
+//  Name        :DeleteFirst
+//  Input       :PPNODE, PPNODE
+//  Returns     :void
+//  Description :Deletes element at first position
+//  Author      :Pranav Choudhary
+//  Date        :31 Aug 2020
+//
+////////////////////////////////////////////////////////////
+void DeleteFirst(PPNODE First, PPNODE Last)
+{
+    if(NULL == *First || NULL == *Last)
+    {
+        printf("Linked List is already empty\n");
+        return;
+    }
+    else
+    {
+        PNODE nTemp = *First;
+        *First = nTemp->npNext;
+        free(nTemp);
+    }
+    (*Last)->npNext = *First;
+}// End of DeleteFirst
+
+////////////////////////////////////////////////////////////
+//
+//  Name        :DeleteLast
+//  Input       :PPNODE, PPNODE
+//  Returns     :void
+//  Description :deletes element at last position
+//  Author      :Pranav Choudhary
+//  Date        :31 Aug 2020
+//
+////////////////////////////////////////////////////////////
+void DeleteLast(PPNODE First, PPNODE Last)
+{
+    if(NULL == *First || NULL == *Last)
+    {
+        printf("Linked List is already empty\n");
+        return;
+    }
+    else if((*First)->npNext == NULL && (*Last)->npNext == NULL)
+    {
+        free(*First);
+        *First = NULL;
+        *Last = NULL;
+    }
+    else
+    {
+        PNODE nTemp = *First;
+        while(nTemp->npNext != *Last)
+        {
+            nTemp = nTemp->npNext;
+        }
+        *Last = nTemp;
+        free((*Last)->npNext);
+        (*Last)->npNext = *First;
+    }
+    
+}// End of DeleteLast
+
+////////////////////////////////////////////////////////////
+//
+//  Name        :DeleteAtPos
+//  Input       :PPNODE, PPNODE, int
+//  Returns     :void
+//  Description :Deletes element from desired position
+//  Author      :Pranav Choudhary
+//  Date        :31 Aug 2020
+//
+////////////////////////////////////////////////////////////
+void DeleteAtPos(PPNODE First, PPNODE Last, int iPos)
+{
+    int iSize = CountEls(*First, *Last);
+    if(iPos > iSize || iPos < 1 || NULL == *First || NULL == *Last)
+    {
+        printf("Invalid Position Entered or Linked list is empty\n");
+        return;
+    }
+    else if(iPos == 1)
+    {
+        DeleteFirst(First, Last);
+        return;
+    }
+    else if(iPos == iSize)
+    {
+        DeleteLast(First, Last);
+        return;
+    }
+    else
+    {
+        PNODE nTemp = *First;
+        PNODE nTarget = NULL;
+        int i = 0;
+
+        for (i = 1; i < iPos - 1; i++)
+        {
+            nTemp = nTemp->npNext;
+        }
+        nTarget = nTemp->npNext;
+        nTemp->npNext = nTarget->npNext;
+        free(nTarget);
+    }   
+}// End of DeleteAtPos
