a9c5b8a877cf1c8559f447a89cefbd04192e1189
PhoeniX-d <prnv24choudhary@gmail.com>
2020-09-10 00:55:10 +0530

    Problems On SCLL part 1

--- Problems On Data Structures/SCLL_CircularLL_Problems1.cpp
+++ Problems On Data Structures/SCLL_CircularLL_Problems1.cpp
@@ -2,8 +2,11 @@
 *   Implementaion of Singly CIrcular Linked List in C
 */
 #include<stdio.h>
+
+#define SUCCESS         0x0000
 #define EMPTY           0x1000
 #define NOTFOUND        0x0100
+#define INVALID_POS     0x0010
 
 // structure of node of circular linked list
 typedef class Node
@@ -34,6 +37,10 @@ public:
     int SecOccur(int);
     int LastOccur(int);
     int SecLastOccur(int);
+    int ListCat(Singly_CircularLL &);
+    int ListNCat(Singly_CircularLL &, int);
+    bool IsPalindrome();
+    void ListSortAsc();
 };
 
 ////////////////////////////////////////////////////////////
@@ -355,13 +362,176 @@ int Singly_CircularLL::SecLastOccur(int iNum)
     }
 }// end of SecLastOccur
 
+/////////////////////////////////////////////////////////////////////
+//
+//  Name        :FirstOccur
+//  Input       :int
+//  Returns     :int
+//  Description :Finds first occurance of el and returns its position
+//  Author      :Pranav Choudhary
+//  Date        :9 Sept 2020
+//
+/////////////////////////////////////////////////////////////////////
+int Singly_CircularLL::ListCat(Singly_CircularLL &List2)
+{
+    if(NULL == List2.Head)
+    {
+        return EMPTY;
+    }
+
+    PNODE npTemp1 = this->Tail;
+    PNODE npTemp2 = List2.Head;
+    
+    do
+    {
+        this->InsertLast(npTemp2->iData);
+        npTemp2 = npTemp2->npNext;
+    } while (npTemp2 != List2.Tail->npNext);
+    return SUCCESS;
+}
+
+/////////////////////////////////////////////////////////////////////
+//
+//  Name        :FirstOccur
+//  Input       :int
+//  Returns     :int
+//  Description :Finds first occurance of el and returns its position
+//  Author      :Pranav Choudhary
+//  Date        :9 Sept 2020
+//
+/////////////////////////////////////////////////////////////////////
+int Singly_CircularLL::ListNCat(Singly_CircularLL &List2, int iN)
+{
+    if(NULL == List2.Head)
+    {
+        return EMPTY;
+    }
+    if(iN > List2.iCount || iN < 1)
+    {
+        return INVALID_POS;
+    }
+
+    PNODE npTemp1 = this->Tail;
+    PNODE npTemp2 = List2.Head;
+    
+    do
+    {
+        this->InsertLast(npTemp2->iData);
+        iN--;
+        npTemp2 = npTemp2->npNext;
+    } while (npTemp2 != List2.Tail->npNext && iN != 0);
+    return SUCCESS;
+}
+
+///////////////////////////////////////////////////////////////////////
+//
+//  Name        :IsPalindrome
+//  Input       :void
+//  Returns     :bool
+//  Description :Checks  whether list is palindrome or not
+//  Author      :Pranav Choudhary
+//  Date        :9 Sept 2020
+//
+///////////////////////////////////////////////////////////////////////
+bool Singly_CircularLL::IsPalindrome()
+{
+    if(Head == NULL)
+    {
+        return false;
+    }
+    
+    int *iArr = new int[iCount];
+    int i = 0, j = 0;
+    PNODE cTemp = Head;
+    
+    do
+    {
+        iArr[i++] = cTemp->iData;
+        cTemp = cTemp->npNext;
+    } while (cTemp != Tail->npNext);
+
+    i--;
+    j = 0;
+    while(j < i)
+    {
+        if(iArr[i] != iArr[j])
+        {
+            break;
+        }
+        i--;
+        j++;
+    }
+    if(j >= i)
+        return true;
+    else
+        return false;
+}
+
+///////////////////////////////////////////////////////////////////////
+//
+//  Name        :ListSortAsc
+//  Input       :void
+//  Returns     :bool
+//  Description :Sorts list in ascending order
+//  Author      :Pranav Choudhary
+//  Date        :9 Sept 2020
+//
+///////////////////////////////////////////////////////////////////////
+void Singly_CircularLL::ListSortAsc()
+{
+    if(Head == NULL && Head->npNext == NULL)
+    {
+        printf("List is Empty");
+        return;
+    }
+
+    PNODE npTemp1 = Head, npTemp2 = Head->npNext, npTemp3 = NULL;
+
+    do
+    {
+        if(npTemp1->iData > npTemp2->iData)
+        {
+            if(npTemp1 == Head && npTemp2 == Head->npNext)
+            {
+                npTemp1->npNext = npTemp2->npNext;
+                npTemp2->npNext = Head;
+                Head = npTemp2;
+                Tail->npNext = Head;
+            }
+            else
+            {
+                npTemp3 = Head;
+                while(npTemp3->npNext != npTemp1)
+                {
+                    npTemp3 = npTemp3->npNext;
+                }
+                npTemp3->npNext = npTemp2;
+                npTemp1->npNext = npTemp2->npNext;
+                npTemp2->npNext = npTemp1;
+            }
+            npTemp1 = Head;
+            npTemp2 = npTemp1->npNext;
+        }
+        else
+        {
+            npTemp1 = npTemp1->npNext;
+            npTemp2 = npTemp1->npNext;
+        }
+    } while (npTemp2 != Head);
+    Tail = npTemp1;
+}
+
+
 // Entry point
 int main()
 {
-    printf("\n----------Singly Circular Linked List----------\n\n");
+    printf("\n----------Singly Circular Linked List Problems 1----------\n\n");
     int iRet = 0, iNum = 0;
-    Singly_CircularLL scllObj;
-
+    bool bRet = false;
+    Singly_CircularLL scllObj, scllObj1;
+   
+    /* List 1 */
+    printf("-------------------------------------------------------------\n");
     printf("Enter elements [\'0\' to stop entering input]\n");
     while(true)
     {
@@ -378,7 +548,26 @@ int main()
     iRet = scllObj.CountEls();
     printf("Total Elements:\n%d\n", iRet);
 
-    /* Occurances : Find  total occurance of iNum in list */
+    /* List 2
+    printf("-------------------------------------------------------------\n");
+    printf("Enter elements [\'0\' to stop entering input]\n");
+    while(true)
+    {
+        scanf("%d", &iNum);
+        if(iNum == 0)
+        {
+            break;
+        }
+        scllObj1.InsertLast(iNum);
+    }
+    printf("Linked list:\n");
+    scllObj1.DisplayList();
+
+    iRet = scllObj1.CountEls();
+    printf("Total Elements:\n%d\n", iRet);
+
+
+    /* Occurances : Find  total occurance of iNum in list
     printf("-------------------------------------------------------------\n");
     printf("Enter number for total occurnces\t\t:");
     scanf("%d", &iNum);
@@ -396,7 +585,7 @@ int main()
         printf("Total occurances of %d is\t\t\t:%d\n", iNum, iRet);
     }
 
-    /* FirstOccur : Find first occurance of iNum in list */
+    /* FirstOccur : Find first occurance of iNum in list
     printf("-------------------------------------------------------------\n");
     printf("Enter number for first occurance\t\t:");
     scanf("%d", &iNum);
@@ -415,7 +604,7 @@ int main()
     }
     
 
-    /* LastOccur : Find last occurance of iNum in list */
+    /* LastOccur : Find last occurance of iNum in list
     printf("-------------------------------------------------------------\n");
     printf("Enter number for last occurnce\t\t\t:");
     scanf("%d", &iNum);
@@ -433,7 +622,7 @@ int main()
         printf("Last occurance of %d is at position\t\t:%d\n", iNum, iRet);
     }
 
-    /* SecOccur : Find second occurance of iNum in list */
+    /* SecOccur : Find second occurance of iNum in list
     printf("-------------------------------------------------------------\n");
     printf("Enter number for second occurnce\t\t:");
     scanf("%d", &iNum);
@@ -451,7 +640,7 @@ int main()
         printf("Second occurance of %d is at position\t\t:%d\n", iNum, iRet);
     }
 
-    /* SecLastOccur : Find  last occurance of iNum in list */
+    /* SecLastOccur : Find  last occurance of iNum in list 
     printf("-------------------------------------------------------------\n");
     printf("Enter number for second last occurnce\t\t:");
     scanf("%d", &iNum);
@@ -468,6 +657,61 @@ int main()
     {
         printf("Second last occurance of %d is at position\t:%d\n", iNum, iRet);
     }
+
+    
+    /* ListCat:concats list2 at end of list1
+    printf("-------------------------------------------------------------\n");
+    iRet = scllObj.ListCat(scllObj1);
+    if(iRet == EMPTY)
+    {
+        printf("list is empty\n");
+    }
+    else if(iRet == SUCCESS)
+    {
+        printf("List Concatinated:\n");
+        scllObj.DisplayList();
+        printf("Total Elements:\n%d\n", scllObj.CountEls());
+    }
+
+    /* ListNCat : Concats first N elements from List2 
+    printf("-------------------------------------------------------------\n");
+    printf("Enter number of elements to concat\t\t:");
+    scanf("%d", &iNum);
+    iRet = scllObj.ListNCat(scllObj1, iNum);
+    if(iRet == EMPTY)
+    {
+        printf("list is empty\n");
+    }
+    else if(iRet == INVALID_POS)
+    {
+        printf("Invalid number entered[enter number upto size of List 2]\n");
+    }
+    else if(iRet == SUCCESS)
+    {
+        printf("List Concatinated:\n");
+        scllObj.DisplayList();
+        printf("Total Elements:\n%d\n", scllObj.CountEls());
+    }
+
+    /* IsPalindrome : Checks whether List is palindrome or not
+    printf("-------------------------------------------------------------\n");
+    bRet = scllObj.IsPalindrome();
+    if(bRet == false)
+    {
+        printf("List is Not Palindrome\n");
+    }
+    else
+    {
+        printf("List is Palindrome\n");
+    }
+
+    /* ListSortAsc:Sorts list in ascending order */
+    printf("------------------------------------------------------\n");
+    printf("Linked In Sorted order\n");
+    scllObj.ListSortAsc();
+    scllObj.DisplayList();
+    printf("Total Elements List 1\t:%d\n", scllObj.CountEls());
+
     printf("-------------------------------------------------------------\n");
     return 0;
  }
