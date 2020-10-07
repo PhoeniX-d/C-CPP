037ed1b69720368868f10b27840ea7db4ede01cd
Pranav Choudhary <57325714+PhoeniX-d@users.noreply.github.com>
2020-10-07 13:38:21 +0530

    Matrix Programs

---
+++ Problems On Matrix/IsBinaryMatrix.c
@@ -0,0 +1,131 @@
+/*
+*   Program to check whether matrix is binary matrix(contains only 0's and 1's)
+*   or not
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#define TRUE 1
+#define FALSE 0
+typedef int BOOL;
+
+BOOL IsBinary(int **, int, int);
+
+int main()
+{
+    // Code
+    int iRow = 0, iCol = 0, i = 0, j = 0;
+    BOOL bRet = FALSE;
+    int **iArr = NULL;
+
+    printf("Enter the total number of rows in a Matrix\t:");
+    scanf("%d", &iRow);
+    printf("Enter the total number of colums in a Matrix\t:");
+    scanf("%d", &iCol);
+
+    // Updator
+    if (iRow < 0)
+        iRow = -iRow;
+    if (iCol < 0)
+        iCol = -iCol;
+
+    iArr = (int **)malloc(sizeof(int *) * iRow);
+    if (iArr == NULL)
+    {
+        printf("Unable to allocate memory\n");
+        return -1;
+    }
+
+    // Allocation
+    for (int i = 0; i < iRow; i++)
+    {
+        iArr[i] = (int *)malloc(sizeof(int) * iCol);
+        if (iArr[i] == NULL)
+        {
+            printf("Unable to allocate memory\n");
+            return -1;
+        }
+    }
+
+    // Initialization
+    printf("Enter the numbers\n");
+    for (i = 0; i < iRow; i++)
+    {
+        for (j = 0; j < iCol; j++)
+        {
+            scanf("%d", &iArr[i][j]);
+        }
+    }
+
+    printf("Matrix\n");
+    for (i = 0; i < iRow; i++)
+    {
+        for (j = 0; j < iCol; j++)
+        {
+            printf("%-3d", iArr[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Usage
+    bRet = IsBinary(iArr, iRow, iCol);
+    if (bRet == TRUE)
+    {
+        printf("\nEnterd Matrix is a Binary Matrix\n");
+    }
+    else
+    {
+        printf("\nEnterd Matrix is not a Binary Matrix\n");
+    }
+
+    // Deallocation
+    for (i = 0; i < iRow; i++)
+    {
+        free(iArr[i]);
+    }
+    free(iArr);
+    return (0);
+}
+
+/////////////////////////////////////////////////////////////
+//
+//  Name        :IsBinary
+//  Input       :int[][], int, int
+//  Returns     :int
+//  Description :Checks whether entered matrix is binary
+//               matrix or not
+//  Author      :Pranav Choudhary
+//  Date        :7 Oct 2020
+//
+/////////////////////////////////////////////////////////////
+BOOL IsBinary(int *iArr[], int iRow, int iCol)
+{
+    if (iArr == NULL)
+    {
+        printf("Invalid Inputs\n");
+        return FALSE;
+    }
+    if (iRow < 0)
+        iRow = -iRow;
+    if (iCol < 0)
+        iCol = -iCol;
+
+    int i = 0, j = 0;
+    BOOL bFlag = TRUE;
+    for (i = 0; i < iRow; i < i++)
+    {
+        for (j = 0; j < iCol; j++)
+        {
+            if (iArr[i][j] != 1 && iArr[i][j] != 0)
+            {
+                bFlag = FALSE;
+                break;
+            }
+        }
+    }
+
+    if (bFlag == FALSE)
+    {
+        return FALSE;
+    }
+    return TRUE;
+}
